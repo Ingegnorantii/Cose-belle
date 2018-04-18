bf6f868f5be8e7b0753f2a1c83e8a20485b15f57
GaspaMasco <38378139+GaspaMasco@users.noreply.github.com>
2018-04-18 21:05:22 +0200

    Create Triangolo_di_Tartaglia.c

---
+++ Programmi Gaspare/Triangolo_di_Tartaglia.c
@@ -0,0 +1,38 @@
+
+#include <stdio.h>
+
+int main()
+{
+    int numrig, numelem, num_;
+    int i, j;
+    int a;
+    
+    printf("A quale riga del triangolo di Tartaglia vuoi arrivare?: ");
+    scanf("%d", &numrig);
+    
+    num_= numrig;
+    
+    for (i = 0; i < numrig; i++){
+    
+        for (j = 0; j < num_; j++)
+            
+            printf(" ");
+       
+             
+             for (j = 0; j <= i; j++){
+            
+                 if((j == 0) || (j == i))
+                     
+                     a = 1;
+            	 else
+                     a = a*(i-j+1)/j;
+                 
+                 printf(" %d", a);
+             }
+        
+        	num_--;
+        	printf("\n");
+    }
+                 
+    return 0;
+}
