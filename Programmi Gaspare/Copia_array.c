9632d50890c23c3ec1df0783b9c352e19fcd3bb9
GaspaMasco <38378139+GaspaMasco@users.noreply.github.com>
2018-05-30 21:34:00 +0200

    Update Copia_array.c

--- Programmi Gaspare/Copia_array.c
+++ Programmi Gaspare/Copia_array.c
@@ -1,1 +1,46 @@
+#include <stdio.h>
 
+#define SIZE 5
+
+void CopyArray(int arr1[], int arr2[], int i);
+void PrintArray(int arr[]);
+
+int i; 
+
+int main ()
+{
+    int array1[SIZE] = {0, 1, 2, 3, 4};
+    int array2[SIZE] = {0};
+    
+    for(i = 0; i < 5; i++)
+    {
+        CopyArray(array1, array2, i);
+    }
+    
+    PrintArray(array2);
+    
+    return 0;
+    
+}
+
+void CopyArray(int arr1[], int arr2[], int i)
+{
+    arr2[i] = arr1[i];
+}
+    
+void PrintArray (int arr[])
+{
+    for(i = 0; i < 5; i++)
+    {
+        printf("%d\n", arr[i]);
+    }
+    
+    return;
+}
+    
+    
+    
+    
+    
+        
+   
