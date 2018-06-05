bd32bf0cbbae5eec2c160ab7f73ce8d69e24a254
GaspaMasco <38378139+GaspaMasco@users.noreply.github.com>
2018-06-05 11:49:01 +0200

    Create Funzione_myStrcat.c

---
+++ Programmi Gaspare/Funzione_myStrcat.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+void myStrcat(char *s1, char *s2, char *result);
+
+int main()
+{
+    char stringa1[] = "suca";
+    char stringa2[] = "melo";
+    char result[10];
+    
+    myStrcat(stringa1, stringa2, result);
+    
+    printf("%s", result);
+	
+    return 0;
+}
+
+void myStrcat(char *s1, char *s2, char *result)
+{
+    while(*s1 != '\0')
+    {
+        *result = *s1;
+        result++;
+        s1++;
+            
+    }
+    while(*s2 != '\0')
+    {
+        *result = *s2;
+        result++;
+        s2++;
+    }  
+    
+}
+        
+       
+
+    
+    
